0803292ce24073f05e4d5f3cb5ae8af82d99df51
Nikki Lin <nikkiylin@gmail.com>
2020-03-30 16:33:34 -0700

    initial the project and implemented the Symptoms.js

---
+++ coronavirus-news-app/src/components/Symptoms.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const symptoms = () => {
+    return (
+        <div className='symptoms'>
+            <h3>Symptoms of Coronavirus</h3>
+            <ul>
+                <li>Fever</li>
+                <li>Cough</li>
+                <li>Shortness of breath</li>
+            </ul>
+        </div>
+    )
+}
